60a7bf8e59fb99f96d07ac9baef8314a28c9b1b9
Charles <62277677+gogocharles@users.noreply.github.com>
2020-07-20 19:22:53 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -20,20 +20,7 @@ def getDynamicText(url, host, referer, encoding):
     except:
         return "Mission failed! Error Code: " + str(r.status_code)
 
-
-def getStaticText(url):
-    try:
-        kv = {'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                            'Chrome/80.0.3987.132 Safari/537.36'}
-        r = requests.get(url, headers=kv)
-        r.raise_for_status()
-        r.encoding = r.apparent_encoding
-        return r.text
-    except:
-        return "Mission failed! Error Code: " + str(r.status_code)
-
 if __name__=="__main__":
     u = "https://api.scuinfo.com/api/posts?pageSize=15"
     print(getDynamicText(u, 'api.scuinfo.com', 'https://scuinfo.com/', 'gzip'))
-    print(getStaticText("http://www.baidu.com"))
     print(getDate(1563796861))
